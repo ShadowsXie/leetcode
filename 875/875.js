5e389fc5ee33a98445ecf13f7e030d2c022b6544
ShadowsXie <1084827833@qq.com>
2020-07-31 21:11:58 +0800

    875

--- 875/875.js
+++ 875/875.js
@@ -1,16 +1,25 @@
 //尝试一下 暴力 再优化
 /**
  * 
- * @param {number[]} piles 
+ * @param {number[]} piles  
  * @param {number} H
  * @return {number} 
  */
 let minEatingSpeed = function(piles, H) {
     let lo = 1,
       hi = Math.max(...piles);
-    console.log(hi)
     //求出最大香蕉数，在某把中
+    for (let i = 1;i <= hi; i++){
+        let time = 0
+        for(let j = 0; j < piles.length; j++){
+            time += Math.ceil(piles[j]/i)
+        }
+        if(time <= H){
+            lo = i;
+            break
+        }
+    }
     return lo;
 }
 
-minEatingSpeed([3, 6, 7, 11], 8);
+console.log(minEatingSpeed([30, 11, 23, 4, 20], 5))
